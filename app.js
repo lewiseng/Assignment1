a693ace067291740ac2387f5b6682d36b4f7ac79
Lewis <lewischao9@gmail.com>
2022-02-13 14:24:50 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const typedTextSpan = document.querySelector(".typed-text");
+
+
+const textArray = ["Stream", "Rate", "Share", "Musing"];
+const colorList = ["#b4f06f","#f2ff40","#b5f4ff","#ffccffd7"];
+
+const typingDelay = 200;
+const erasingDelay = 150;
+const newTextDelay = 700; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = textArray[0].length;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length ) {
+        typedTextSpan.style.color = colorList[textArrayIndex];
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex++);
+        setTimeout(type, typingDelay);
+    } 
+    else {
+        if (textArrayIndex<textArray.length-1){
+            setTimeout(erase, newTextDelay);
+        }
+        else{
+            typedTextSpan.classList.add("sign");
+        }
+    }
+}
+
+function erase() {
+	if (charIndex >= 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex--);
+        setTimeout(erase, erasingDelay);
+    } 
+    else {
+        textArrayIndex++;
+        setTimeout(type, typingDelay + 600);
+    }
+}
+
+function addBr() {
+    if (window.matchMedia("(max-width: 768px)").matches) { // If media query matches
+        typedTextSpan.insertAdjacentHTML("afterend", "<br>")
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+    addBr();
+    if(textArray.length){
+        typedTextSpan.style.color = colorList[textArrayIndex];
+        typedTextSpan.textContent += textArray[textArrayIndex];
+        setTimeout(erase, newTextDelay + 250);
+  } 
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
